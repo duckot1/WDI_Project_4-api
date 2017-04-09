72e80ddfd71110597e1431f76a4740d51290ccae
Tom <tom.duckworth91@gmail.com>
2017-04-09 17:45:32 +0100

    review bug fixed in the model

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,20 +5,20 @@ class Review < ApplicationRecord
   before_create :user_rating
 
   def user_rating
-    @usersReviews = Review.where({ user_id: self.user_id })
-    p @usersReviews
-    ratings = []
-    @usersReviews.each do |review|
-      ratings.push(review.rating)
-    end
-    p ratings
-    average = ratings.inject{ |sum, el| sum + el }.to_f / ratings.size
-    p average
     @user = User.find_by_id(self.user_id)
-    p @user
-    @user.rating = average
-    p @user
-    @user.save
+    if @user.rating == nil
+      @user.rating = self.rating
+      @user.save
+    else
+      @usersReviews = Review.where({ user_id: self.user_id })
+      ratings = [self.rating]
+      @usersReviews.each do |review|
+        ratings.push(review.rating)
+      end
+      average = ratings.inject{ |sum, el| sum + el }.to_f / ratings.size
+      @user.rating = average
+      @user.save
+    end
   end
 
 end
